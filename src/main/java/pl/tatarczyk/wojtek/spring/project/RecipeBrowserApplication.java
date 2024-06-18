f4e3c57e677d41652cdfbe82d5f09c9819d8f54f
lata1234567 <wojtek.tatarczyk@wp.pl>
2024-06-18 20:10:11 +0200

    first commit

---
+++ src/main/java/pl/tatarczyk/wojtek/spring/project/RecipeBrowserApplication.java
@@ -0,0 +1,13 @@
+package pl.tatarczyk.wojtek.spring.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeBrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeBrowserApplication.class, args);
+	}
+
+}
